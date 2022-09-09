0128cf2aa438bcca48c904382c0172f1718bbb24
sergiokhark <sergio.khark@gmail.com>
2022-09-09 11:57:42 +0300

    Add/Edit in new page v.3

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -10,68 +10,71 @@
     <v-card-actions>
       <v-spacer></v-spacer>
       <v-btn to="/users" color="blue darken-1"> Cancel </v-btn>
-      <v-btn v-if="!createBtn" to="/users" color="blue darken-1" @click="addUser"> Create </v-btn>
-      <v-btn v-if="createBtn" to="/users" color="blue darken-1" @click="save"> Update </v-btn>
+      <v-btn
+        v-if="!createBtn"
+        to="/users"
+        color="blue darken-1"
+        @click="addUser"
+      >
+        Create
+      </v-btn>
+      <v-btn v-if="createBtn" to="/users" color="blue darken-1" @click="save">
+        Update
+      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
 import axios from "axios";
+import { ref } from "vue";
 import ModalUsersFields from "~~/components/modalDialog/ModalUsersFields.vue";
-import useAddUser from '~~/hooks/users/useAddUser'
-import useEditUser from '~~/hooks/users/useEditUser'
+import useAddUser from "~~/hooks/users/useAddUser";
+import useEditUser from "~~/hooks/users/useEditUser";
 
 export default {
   components: {
-    ModalUsersFields
-  },
-  setup() {
-    const { addUser } = useAddUser()
-    const { save } = useEditUser()
-
-    return {
-      addUser, save
-    }
+    ModalUsersFields,
   },
   data() {
     return {
       user: {
-          name: '',
-          username: '',
-          website: '',
-          address: {
-            city: '',
-            street: '',
-            suite: ''
-          }
+        name: "",
+        username: "",
+        website: "",
+        address: {
+          city: "",
+          street: "",
+          suite: "",
         },
-      title: "",
-      createBtn: true
+      },
     };
   },
   async mounted() {
-    if (this.$route.params.id !== 'create') {
-      this.title = "Edit user"
+    if (this.$route.params.id !== "create") {
+      this.title = "Edit user";
       const res = await axios.get(
         "https://jsonplaceholder.typicode.com/users/" + this.$route.params.id
       );
-      
-      this.user = res.data
+      this.user = res.data;
     } else {
-      this.title = "Add user"
-      this.createBtn = false
-      this.user = {
-        name: '',
-        username: '',
-        site: '',
-        address: {
-          city: '',
-          street: '',
-          suite: ''
-        }
-      }
+      this.title = "Add user";
+      this.createBtn = false;
     }
-  }, 
+  },
+  setup() {
+    const { addUser } = useAddUser();
+    const { save } = useEditUser();
+    const title = ref("");
+    const createBtn = ref(true);
+    
+    return {
+      addUser,
+      save,
+      title,
+      createBtn
+    };
+  },
+  
 };
 </script>
