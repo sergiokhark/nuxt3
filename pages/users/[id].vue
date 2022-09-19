65b9f445f57502b7d30135e9353c6d04cb143c12
sergiokhark <sergio.khark@gmail.com>
2022-09-19 08:56:56 +0300

    newUser in useAddUser fail

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -18,32 +18,7 @@ export default {
   components: {
     ModalUsersFields,
   },
-  // data() {
-  //   return {
-  //     user: {
-  //       name: "",
-  //       username: "",
-  //       website: "",
-  //       address: {
-  //         city: "",
-  //         street: "",
-  //         suite: "",
-  //       },
-  //     },
-  //   };
-  // },
-  // async mounted() {
-  //   if (this.$route.params.id !== "create") {
-  //     this.title = "Edit user";
-  //     const res = await axios.get(
-  //       "https://jsonplaceholder.typicode.com/users/" + this.$route.params.id
-  //     );
-  //     this.user = res.data;
-  //   } else {
-  //     this.title = "Add user";
-  //     this.createBtn = false;
-  //   }
-  // },
+  
   setup(props) {
     const { addUser } = useAddUser(props);
     const { save } = useEditUser();
@@ -79,6 +54,7 @@ export default {
       save,
       title,
       createBtn,
+      user
     };
   },
 };
