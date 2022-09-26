c41297a6157b77b5869b0124e0a41dfb991b5d79
sergiokhark <sergio.khark@gmail.com>
2022-09-26 09:32:58 +0300

    Add statistics

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,50 @@
 <template>
-  <v-row>
-    <v-col cols="12" sm="12" lg="8"></v-col>
-    <v-col cols="12" sm="12" lg="4"></v-col>
-    <v-col cols="12" sm="12" lg="4">
-      
-      <div class="mb-7"></div>
-      
-    </v-col>
-    <v-col cols="12" sm="12" lg="8"></v-col>
-  </v-row>
+ <v-card>
+    <v-card-text>
+      <br />
+      <h2 class="mb-7">Statistics</h2>
+      <v-table>
+        <thead>
+          <tr>
+            <th class="text-left">Title</th>
+            <th class="text-left">Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Users</td>
+            <td>{{ users.length }}</td>
+          </tr>
+          <tr>
+            <td>Products</td>
+            <td>{{ products.length }}</td>
+          </tr>
+          <tr>
+            <td>Orders</td>
+            <td>{{ orders.length }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </v-card-text>
+  </v-card>
 </template>
 
-<script setup lang="ts">
+<script>
+import { useFetchUsers } from '~/hooks/useActionUsers'
+import { useFetchProducts } from '~/hooks/useActionProducts'
+import { useFetchOrders } from '~/hooks/useActionOrders'
 
+export default {
+  setup() {
+    const { users } = useFetchUsers()
+    const { products } = useFetchProducts()
+    const { orders } = useFetchOrders()
+
+    return {
+      users,
+      products,
+      orders
+    }
+  }
+}
 </script>
